3f7596e1778c05555286d2d47515a25cb1dbd613
Owen <oclyke@gmail.com>
2018-04-10 14:10:16 -0700

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -1,1 +1,30 @@
+/*
+gpio.c
+
+Owen Lyke
+April 2018
+
+GPIO functionality for ATmega 1248p
+*/
+
+// Register definitions
+#define MCUCR 0x55
+
+#define PORTA 	0x22
+#define PORTB 	0x25
+#define PORTC 	0x28
+#define PORTD 	0x2B
+
+#define PINA 	0x20
+#define PINB 	0x23
+#define PINC	0x26
+#define PIND	0x29
+
+#define DDRA	0x21
+#define DDRB	0x24
+#define DDRC	0x27
+#define DDRD	0x28
+
+
+
 
