021305a9daee890aa8a9f107d66c4e44682fdcb8
Owen <oclyke@gmail.com>
2018-04-10 14:21:00 -0700

    Update gpio.h

--- gpio.h
+++ gpio.h
@@ -10,6 +10,21 @@ Just a simple GPIO library for the ATmega1248p, for use in the CryoDust project
 
 
 
+#define OUTPUT  1
+#define INPUT   0
+
+#define HIGH    1
+#define LOW     0
+
+
+
+void pinMode(uint8_t num, uint8_t mode);
+void digitalWrite(uint8_t num, uint8_t value);
+void digitalRead(uint8_t num);
+
+
+
+
 
 
 
