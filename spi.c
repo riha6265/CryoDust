b0e0f5dcd553f78b3fb3d6db30be5eb047564451
riha6265 <riha6265@colorado.edu>
2018-04-12 11:02:45 -0600

    Updating the spi files (dont work)

--- spi.c
+++ spi.c
@@ -1,1 +1,27 @@
+#include "spi.h"
 
+void initSpi(void){
+	 //Set MOSI and SCK output, all others input 
+	DDRB = (1<<DDB5)|(1<<DDB7);
+	//Enable SPI, Master, set clock rate fck/16 
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+}
+
+void transmitByte(char data){
+	//Start transmission
+	SPDR = data;
+	//Wait for transmission complete
+	while(!(SPSR & (1<<SPIF)))
+	;
+}
+
+void transmit(uint8_t * sentence, uint8_t size){
+	volatile int i = 0;
+	for(i; i < size; i++){
+		transmitByte(*sentence);
+	}
+}
+
+char receive(){
+	return SPDR;
+}
--- spi.h
+++ spi.h
@@ -1,1 +1,22 @@
+/*
+ * spi.h
+ *
+ * Created: 4/12/2018 5:27:48 AM
+ *  Author: Riley
+ */ 
 
+#ifndef SPI_H
+#define SPI_H
+
+#include <avr/io.h>
+
+void initSpi(void);
+//Initializes the SPI interface
+
+void transmit(uint8_t * sentence, uint8_t size);
+//Transmits an array of chars of size
+
+char receive(void);
+//Return the SPI data buffer
+
+#endif
