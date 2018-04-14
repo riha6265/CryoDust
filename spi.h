986b65c5c60bb04e469797a7b808dac812dd4189
Owen <oclyke@gmail.com>
2018-04-13 18:16:35 -0700

    Update spi.h

--- spi.h
+++ spi.h
@@ -1,1 +1,22 @@
+/*
+ * spi.h
+ *
+ * Created: 4/12/2018 5:27:48 AM
+ *  Author: Riley
+ */ 
 
+#ifndef SPI_H
+#define SPI_H
+
+#include <avr/io.h>
+
+void SPI_init(uint8_t polarity, uint8_t phase);
+//Initializes the SPI interface
+
+uint8_t * SPI_transmit(uint8_t * sentence, uint8_t size);
+//Transmits an array of chars of size
+
+uint8_t SPI_receive(void);
+//Return the SPI data buffer
+
+#endif
